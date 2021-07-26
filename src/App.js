4eab2b6d42a229816d97e14cecac82bb857cde38
nattawat-wib <nattawat.wib@gmail.com>
2021-07-26 23:29:43 +0700

    end of day one

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { Fragment } from 'react';
 
 function App() {
   return (
-    <Fragment>
+    <>
       <Navbar/>
       <main>
 
       </main>
       <Footer/>
-    </Fragment>
+    </>
   );
 }
 
