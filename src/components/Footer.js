4923c2e3aecf80eb9b903f57bc3efae2eac18cb0
nattawat-wib <nattawat.wib@gmail.com>
2021-07-26 23:21:59 +0700

    setup

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <h1>
+                test footer
+            </h1>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>
+                test navbar
+            </h1>
+        </nav>
+    )
+}
+
+export default Navbar
